2fbff38a674d1c752c3f2fc4f6d60c39a1e04de3
anthonyalchemy <anthony@alchemytechs.com>
2019-07-29 11:38:21 +0100

    Add project files.

---
+++ DataTypes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Alchemy Academy"; //plain text
+            char grade = 'A'; // one character and note single quotation
+
+            int age = 30; //int no decimal points and no quotation 
+            double gpa = 3.2;
+            //float, double, decimal three data types for decimal numbers least accurate to most accurate 
+
+            bool isMale = true; //will tell if someone is a male by true or false 
+
+
+            Console.ReadLine();
+        }
+    }
+}
